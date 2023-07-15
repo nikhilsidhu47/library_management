5ed4a03e5f62da70eaef870469a8987c8ae908b5
Nikhil <71748297+nikhilsidhu47@users.noreply.github.com>
2023-07-15 23:01:29 +0530

    Update Constant.java

--- src/main/java/com/nagarro/library_management/constants/Constant.java
+++ src/main/java/com/nagarro/library_management/constants/Constant.java
@@ -1,11 +1,11 @@
 package com.nagarro.library_management.constants;
 
 public class Constant {
-	public static final String HIBERNATE_USERNAME="root";
-	public static final String HIBERNATE_PASSWORD="nikhilroot";
+	public static final String HIBERNATE_USERNAME="nikhilroot";
+	public static final String HIBERNATE_PASSWORD="Ritiva@taken";
 	public static final String HIBERNATE_DIALECT="org.hibernate.dialect.MySQLDialect";
 	public static final String HIBERNATE_DRIVER="com.mysql.cj.jdbc.Driver";
-	public static final String HIBERNATE_URL="jdbc:mysql://beanstalk-library-db.cedif7kttgpl.us-east-1.rds.amazonaws.com:3306/library_login";
+	public static final String HIBERNATE_URL="jdbc:mysql://assignment-server-nikhil.mysql.database.azure.com/:3306/library_login";
 	public static final String HIBERNATE_HBM2DDL_AUTO="update";
 	public static final String HIBERNATE_SHOW_SQL="false";
 	public static final String BOOK_API = "http://localhost:8082/api/books";
